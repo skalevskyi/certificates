9324098a96f47f2b9d71f6a09ef9fc315062b72c
skalevskyi <sskalevskyi@gmail.com>
2024-08-15 23:37:11 +0200

    add all certificates

--- script.js
+++ script.js
@@ -45,3 +45,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('slider').addEventListener('change', toggleTheme);
 });
+// -------------------details-------------
